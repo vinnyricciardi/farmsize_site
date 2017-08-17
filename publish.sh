4e4c97e000b6b8cbe592f92747aa45c404ee7429
vinnyricciardi <vinnyricciardi@gmail.com>
2017-08-17 11:15:48 -0400

    Update Slides

--- publish.sh
+++ publish.sh
@@ -54,8 +54,8 @@ git push --force origin master gh-pages
 #git checkout -B gh-pages
 #cp -r /tmp/workspace/* .
 #git add -A .
-#git commit -m "Update Slides"
-#git push --force origin gh-pages
+git commit -m "Update Slides"
+git push --force origin gh-pages
 #git checkout master
 rm -rf /tmp/workspace
 
