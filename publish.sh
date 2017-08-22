287b0801801993af26970bd6f818f872cc03fc17
vinnyricciardi <vinnyricciardi@gmail.com>
2017-08-22 12:30:21 -0400

    Update Slides

--- publish.sh
+++ publish.sh
@@ -45,7 +45,7 @@ for f in "${arr[@]}"; do
    #cd ..
 done
 
-cp Notebooks/pivottablejs.html Html/pivottablejs.html 
+cp Notebooks/pivottablejs.html Html/pivottablejs.html
 
 # Push the updates to gh-pages
 #mkdir -p /tmp/workspace
